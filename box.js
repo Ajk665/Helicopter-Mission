8eb6c532804724a4eae4c6e7350ae76203cda0d7
Ajk665 <72042993+Ajk665@users.noreply.github.com>
2020-10-16 06:48:30 +0100

    Add files via upload

---
+++ box.js
@@ -0,0 +1,26 @@
+class Box{
+    constructor(x,y,width,height){
+    
+        
+        var options = {
+            restitution: 0,
+            friction: 1,
+            density: 1
+        }
+
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+
+        World.add(world,this.body);
+    
+
+    }
+    display(){
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill("red");
+        rect(pos.x,pos.y,this.width,this.height);
+
+    }
+}
